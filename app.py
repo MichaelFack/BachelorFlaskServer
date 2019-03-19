f15357d2e1a127a97c9eb9247828ee781f72d773
michael <michael.f.sorensen@gmail.com>
2019-03-19 18:02:55 +0100

    Server refactored for future impl. of name differentiation. Redirects on 413 to main now. Added max content length arg in app config: 1 GB. Discovered that nginx also has an upper limit.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from werkzeug.utils import secure_filename
 curr_path = os.getcwd()
 UPLOAD_FOLDER = curr_path + '/UPLOADS/'
 RESOURCE_FOLDER = curr_path + '/resources'
-ALLOWED_EXTENSIONS = set(['txt', 'png'])
+ALLOWED_EXTENSIONS = {'txt', 'png', 'gif', 'pdf', 'wav'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -21,7 +21,8 @@ def nothing():
 def get_file_list():
     if request.method == 'GET':
         files = os.listdir(UPLOAD_FOLDER)
-        return jsonify(files)
+        accurate_names = set(map(latest_filename_version, files))
+        return jsonify(list(accurate_names))
 
 
 @app.route('/upload_file', methods=['POST'])
@@ -29,41 +30,102 @@ def upload_file():
     if request.method == 'POST':
 
         if len(request.files) == 0:  # No file
-            flash('No file sent along with request.')
-            return redirect('/', code=302)
+            return redirect('/', code=400)
 
         file = request.files['file']
-        if acceptable_file(file.filename) and acceptable_file(secure_filename(file.filename)):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            redirect('/list_files', code=302)
-        else:
-            flash("Invalid file. Can't accept.")
-        return redirect('/', code=302)
+        filename_unchecked = file.filename
+        sec_filename = secure_filename(filename_unchecked)
+
+        if acceptable_file(filename_unchecked) and acceptable_file(sec_filename):
+
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename_to_server_side_name(sec_filename)))
+            return redirect('/list_files', code=302)  # TODO: Perhaps should be changed.
+
+        else:  # Unacceptable filename.
+
+            return redirect('/', code=400)
 
 
 @app.route('/get_file/<string:filename_unchecked>', methods=['GET'])
 def get_file(filename_unchecked):
-    if request.method == 'GET' and acceptable_file(filename_unchecked):
-        filename = secure_filename(filename_unchecked)
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+    sec_filename = secure_filename(filename_unchecked)
+
+    if request.method == 'GET' and acceptable_file(filename_unchecked)\
+            and acceptable_file(sec_filename):
+
+        latest_filename = latest_filename_version(sec_filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], latest_filename)
+
         if os.path.exists(file_path) and os.path.isfile(file_path):
-            return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+            return send_from_directory(app.config['UPLOAD_FOLDER'], latest_filename)
         else:
-            flash("No such file")
-            return redirect('/', code=404)
+            return redirect('/', code=400)
     else:
-        flash("Request not understood. Try checking the url.")
         return redirect('/', code=400)
 
 
+# TODO: (maybe) move, (maybe) archive, name differentiation
+
+
+@app.route('/rename_file', methods=['POST'])
+def rename_file():
+    if request.method == 'POST':
+
+        new_filename_unchecked = request.args.get("old_name", None)
+        old_filename_unchecked = request.args.get("new_name", None)
+
+        if new_filename_unchecked is None \
+                or old_filename_unchecked is None:  # Should have args in request.
+            return redirect('/', code=400)
+
+        sec_new_filename = secure_filename(new_filename_unchecked)
+        sec_old_filename = secure_filename(old_filename_unchecked)  # get new secure filenames.
+
+        latest_filename = latest_filename_version(sec_new_filename)  # what's the real name of the file?
+
+        if not acceptable_file(new_filename_unchecked) \
+                or not acceptable_file(old_filename_unchecked) \
+                or not acceptable_file(sec_new_filename) \
+                or not acceptable_file(sec_old_filename) \
+                or not os.path.exists(UPLOAD_FOLDER + latest_filename) \
+                or not os.path.isfile(UPLOAD_FOLDER + latest_filename):
+            return redirect('/', code=400)  # Something with the name is bad, or the file doesn't exist.
+        # valid names and file exists.
+        os.rename(latest_filename,
+                  filename_to_server_side_name(sec_new_filename))
+        return redirect('/', code=200)
+    else:
+        return redirect('/', code=400)  # Bad request.
+
+
+@app.errorhandler(413)
+def request_entity_too_large(error):
+    return redirect('/', 413)
+
+
+# used in implementing name differentiation - whenever we store a file append current local time.
+def latest_filename_version(filename):
+    return filename  # TODO: get latest filename version
+
+
+def filename_to_server_side_name(filename):
+    return filename  # TODO: name differentiation
+
+
+def server_side_name_to_filename(server_side_name):
+    return server_side_name  # TODO: reverse name differentiation
+
+
 def acceptable_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS and len(filename.rsplit('/')) == 1
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS and len(
+        filename.rsplit('/')) == 1
 
 
 if __name__ == '__main__':
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
+    app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024
 
     if not os.path.exists(UPLOAD_FOLDER):
         os.mkdir(UPLOAD_FOLDER)
